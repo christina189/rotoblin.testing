b19acc3582ef830430e383ba2717b9d94e31b71f
Mark Simpson <mjsimpson@gmail.com>
2011-01-08 01:01:04 +0000

    Adding files and re-instating the mercurial repository as being up to date (ish!)
    NOTE: Current implementation is bugged (molotovs, pipes, cans and pills are sometimes evading removal), and this is not the release code for 0.7.5.  Do not use this current version; stick with the released version of 0.7.5 for the moment.
    
    Changes include:
    + Added (optional) hardcore mode for more challenging gameplay
    	+ Removal of all non safe room & finale pills 		(modified healthcontrol.sp)
    	+ Pipe, molotov, gas can removal 			(added itemcontrol.sp)
    	+ Melee fatigue 					(added meleefatigue.sp)
    	+ Added new rotoblin_hardcore.cfg
    
    + Added FTP upload batch file for easy testing 			(added deploy.bat -- read the comments)
    
    * Rejigged the directory structure a bit.  Structure is not yet finale as I may have missed a file or two.

---
+++ makefile.bat
@@ -0,0 +1,22 @@
+echo off
+setlocal
+
+set "PLUGIN_DIR=src\scripting\rotoblin"
+set "PLUGIN_NAME=rotoblin.main"
+set "RELEASE_NAME=rotoblin"
+set "SPCOMP=bin\sourcepawn\spcomp.exe"
+
+echo --------------------------------------------
+echo Building %PLUGIN_NAME%.sp ...
+echo --------------------------------------------
+
+"%SPCOMP%" -D%PLUGIN_DIR% "%PLUGIN_NAME%.sp"
+move /Y "%PLUGIN_DIR%\%PLUGIN_NAME%.smx" "%RELEASE_NAME%.smx" 
+
+echo --------------------------------------------
+echo Built %PLUGIN_NAME%.sp as %RELEASE_NAME%.smx
+echo --------------------------------------------
+
+endlocal
+pause
+echo on
